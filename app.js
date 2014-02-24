71b993d45dc10a0110ae8ed78858c13f6045b422
vojacek <pavelvojacek@gmail.com>
2014-02-24 17:40:07 +0100

    progress to console

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var wineListLoader = require('wineListLoader');
 var fs = require('fs');
-wineListLoader.listWinesFromDir('./test/xls').then(
+wineListLoader.listWinesFromDir('./xls').then(
     function(data) {
-        fs.writeFile('data.json', JSON.stringify(data[0].value, null, 4), function (err) {
+        fs.writeFile('data.json', JSON.stringify(data, null, 4), function (err) {
             if (err) throw err;
             console.log('It\'s saved!');
         });
